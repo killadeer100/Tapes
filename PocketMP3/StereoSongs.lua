ca66b0d65f0b6b8743b876ea3f4555a488305f52
killadeer100 <killadeer100@gmail.com>
2023-05-21 13:54:52 -0500

    Update StereoSongs.lua

--- PocketMP3/StereoSongs.lua
+++ PocketMP3/StereoSongs.lua
@@ -7,5 +7,7 @@ songTbl["RebelLeft"] = "https://github.com/killadeer100/Tapes/raw/Tapes/The%20Re
 songTbl["RebelRight"] = "https://github.com/killadeer100/Tapes/raw/Tapes/The%20Rebel%20Path%208-bit%20-%20Right.dfpwm"
 songTbl["RickLeft"] = "https://github.com/killadeer100/Tapes/raw/Tapes/Rick%20Roll%208-bit%20-%20Left.dfpwm"
 songTbl["RickRight"] = "https://github.com/killadeer100/Tapes/raw/Tapes/Rick%20Roll%208-bit%20-%20Right.dfpwm"
+songTbl["HoshinoLeft"] = "https://github.com/killadeer100/Tapes/raw/Tapes/Hoshino%20Kieta%20Yoruni%20-%20Left.dfpwm"
+songTbl["HoshinoRight"] = "https://github.com/killadeer100/Tapes/raw/Tapes/Hoshino%20Kieta%20Yoruni%20-%20Right.dfpwm"
 
 return { songTbl = songTbl }
