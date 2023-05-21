2259df436e05608bfd80a039eac09a8806e67d1a
killadeer100 <killadeer100@gmail.com>
2023-05-21 13:27:39 -0500

    Update StereoSongs.lua

--- PocketMP3/StereoSongs.lua
+++ PocketMP3/StereoSongs.lua
@@ -1,5 +1,11 @@
 local songTbl = {}
-songTbl["MotorswanLeft"] = "https://github.com/killadeer100/Tapes/raw/Tapes/Motorswan%20-%20Left.dfpwm"
-songTbl["MotorswanRight"] = "https://github.com/killadeer100/Tapes/raw/Tapes/Motorswan%20-%20Right.dfpwm"
+songTbl["MotorswanLeft"] = "https://github.com/killadeer100/Tapes/raw/Tapes/Motorswan%208-bit%20Left.dfpwm"
+songTbl["MotorswanRight"] = "https://github.com/killadeer100/Tapes/raw/Tapes/Motorswan%208-bit%20-%20Right.dfpwm"
+songTbl["VLeft"] = "https://github.com/killadeer100/Tapes/raw/Tapes/V%208-bit%20-%20Left.dfpwm"
+songTbl["VRight"] = "https://github.com/killadeer100/Tapes/raw/Tapes/V%208-bit%20-%20Right.dfpwm"
+songTbl["RebelLeft"] = "https://github.com/killadeer100/Tapes/raw/Tapes/The%20Rebel%20Path%208-bit%20-%20Left.dfpwm"
+songTbl["RebelRight"] = "https://github.com/killadeer100/Tapes/raw/Tapes/The%20Rebel%20Path%208-bit%20-%20Right.dfpwm"
+songTbl["RickLeft"] = "https://github.com/killadeer100/Tapes/raw/Tapes/Rick%20Roll%208-bit%20-%20Left.dfpwm"
+songTbl["RickRight"] = "https://github.com/killadeer100/Tapes/raw/Tapes/Rick%20Roll%208-bit%20-%20Right.dfpwm"
 
 return { songTbl = songTbl }
