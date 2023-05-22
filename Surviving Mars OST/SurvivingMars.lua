de93ba3c499db207a8046bf9fa6ac0837939ca7d
killadeer100 <killadeer100@gmail.com>
2023-05-21 23:05:04 -0500

    Add files via upload

---
+++ Surviving Mars OST/SurvivingMars.lua
@@ -0,0 +1,45 @@
+local songArr = {}
+songArr[1 ] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/01%20Ag.dfpwm"
+songArr[2 ] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/02%20Bacl2.dfpwm"
+songArr[3 ] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/03%20Caco3.dfpwm"
+songArr[4 ] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/04%20Ch4.dfpwm"
+songArr[5 ] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/05%20CL.dfpwm"
+songArr[6 ] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/06%20Co2.dfpwm"
+songArr[7 ] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/07%20Cold%20Wave.dfpwm"
+songArr[8 ] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/08%20Fe.dfpwm"
+songArr[9 ] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/09%20Fecl3.dfpwm"
+songArr[10] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/10%20H2o.dfpwm"
+songArr[11] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/11%20H3po4.dfpwm"
+songArr[12] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/12%20Hcl.dfpwm"
+songArr[13] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/13%20He.dfpwm"
+songArr[14] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/14%20Hg.dfpwm"
+songArr[15] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/15%20Main%20Theme%20(Alternative%20Version).dfpwm"
+songArr[16] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/16%20Main%20Theme.dfpwm"
+songArr[17] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/17%20Mgo.dfpwm"
+songArr[18] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/18%20Mno2.dfpwm"
+songArr[19] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/19%20Mo.dfpwm"
+songArr[20] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/20%20Nah.dfpwm"
+songArr[21] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/21%20Ne.dfpwm"
+songArr[22] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/22%20No2.dfpwm"
+songArr[23] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/23%20O2.dfpwm"
+songArr[24] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/24%20Sanctuary.dfpwm"
+songArr[25] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/25%20Sandstorm.dfpwm"
+songArr[26] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/26%20Si02.dfpwm"
+songArr[27] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/27%20So2.dfpwm"
+songArr[28] = "https://github.com/killadeer100/Tapes/raw/Tapes/Surviving%20Mars%20OST/28%20Xe.dfpwm"
+local songArrSize = 1
+local prevSongArrSize = 1
+
+local blurbArr = {}
+local blurbArrSize = 0
+local prevBlurbArrSize = 0
+
+local talkArr = {}
+local talkArrSize = 0
+local prevTalkArrSize = 0
+
+local commArr = {}
+local commArrSize = 0
+local prevCommArrSize = 0
+
+return { songArr = songArr, songArrSize = songArrSize, prevSongArrSize = prevSongArrSize, blurbArr = blurbArr, blurbArrSize = blurbArrSize, prevBlurbArrSize = prevBlurbArrSize, talkArr = talkArr, talkArrSize = talkArrSize, prevTalkArrSize = prevTalkArrSize, commArr = commArr, commArrSize = commArrSize, prevCommArrSize = prevCommArrSize }
